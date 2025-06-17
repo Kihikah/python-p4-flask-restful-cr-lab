7801c40e5130136e7a3f851ef43cdd6a31a0d147
Kariuki Kihikah <kariukikihikah@gmail.com>
2025-06-17 13:39:47 +0300

    REST Create and Retrieve Lab

--- server/app.py
+++ server/app.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python3
 
-from flask import Flask, jsonify, request, make_response
+from flask import Flask, request
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
-
 from models import db, Plant
 
 app = Flask(__name__)
@@ -16,12 +15,44 @@ db.init_app(app)
 
 api = Api(app)
 
+# ----------- Resources -----------
+
 class Plants(Resource):
-    pass
+    def get(self):
+        plants = Plant.query.all()
+        return [plant.to_dict() for plant in plants], 200
+
+    def post(self):
+        data = request.get_json()
+
+        try:
+            new_plant = Plant(
+                name=data['name'],
+                image=data['image'],
+                price=data['price']
+            )
+
+            db.session.add(new_plant)
+            db.session.commit()
+
+            return new_plant.to_dict(), 201
+        except Exception as e:
+            return {"error": str(e)}, 400
 
 class PlantByID(Resource):
-    pass
-        
+    def get(self, id):
+        plant = Plant.query.get(id)
+
+        if plant:
+            return plant.to_dict(), 200
+        return {"error": "Plant not found"}, 404
+
+# ----------- Routes -----------
+
+api.add_resource(Plants, '/plants')
+api.add_resource(PlantByID, '/plants/<int:id>')
+
+# ----------- Run App -----------
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/testing/app_test.py
+++ server/testing/app_test.py
@@ -1,6 +1,8 @@
 import json
 
-from app import app
+#from app import app
+from server.app import app
+
 from models import db, Plant
 
 class TestPlant:
